dbc947965a9dddfda79e7caa998ddf9e3a106965
Laurent Jouanneau <laurent@jelix.org>
2012-05-10 00:22:22 +0200

    better script to update repo and books

--- update_repo_and_books.sh
+++ update_repo_and_books.sh
@@ -2,41 +2,58 @@
 
 # This script can be called by a cron...
 
-cd repositories/en/
-git fetch --all
 
-cd ../../repositories/fr
-git fetch --all
+FORCE=$1
 
-cd ../..
 
-rm -rf books/manual-1.0
-php doc_en/scripts/manage.php gitiwiki~wiki:generateBook manual-1.0 index
+update()
+{
+cd $REPO
+for index in 1 2 3 4 5
+do
+    br=${BRANCH[index]}
+    echo "checkout $br..."
+    
+    git checkout $br
+    OLDREV=`cat .git/refs/heads/$br`
+    git pull origin $br
+    NEWREV=`cat .git/refs/heads/$br`
 
-rm -rf books/manual-1.1
-php doc_en/scripts/manage.php gitiwiki~wiki:generateBook manual-1.1 index
+    book=${BOOK[index]}
+    if [ "$FORCE" != "" -o "$OLDREV" != "$NEWREV" ]; then
+        cd $ROOTPATH
+        rm -rf books/$book
+        php $APP/scripts/manage.php gitiwiki~wiki:generateBook $book index
+        cd $REPO
+    fi
+done
+cd $ROOTPATH
+}
 
-rm -rf books/manual-1.2
-php doc_en/scripts/manage.php gitiwiki~wiki:generateBook manual-1.2 index
+BRANCH[1]="master"
+BRANCH[2]="jelix-1.0"
+BRANCH[3]="jelix-1.1"
+BRANCH[4]="jelix-1.2"
+BRANCH[5]="jelix-1.3"
 
-rm -rf books/manual-1.3
-php doc_en/scripts/manage.php gitiwiki~wiki:generateBook manual-1.3 index
+BOOK[1]="manual-1.4"
+BOOK[2]="manual-1.0"
+BOOK[3]="manual-1.1"
+BOOK[4]="manual-1.2"
+BOOK[5]="manual-1.3"
 
-rm -rf books/manual-1.4
-php doc_en/scripts/manage.php gitiwiki~wiki:generateBook manual-1.4 index
+APP=doc_en
+REPO=repositories/en/jelix-manual-en/
+ROOTPATH=../../..
+update
 
-rm -rf books/manuel-1.0
-php doc_fr/scripts/manage.php gitiwiki~wiki:generateBook manuel-1.0 index
+BOOK[1]="manuel-1.4"
+BOOK[2]="manuel-1.0"
+BOOK[3]="manuel-1.1"
+BOOK[4]="manuel-1.2"
+BOOK[5]="manuel-1.3"
 
-rm -rf books/manuel-1.1
-php doc_fr/scripts/manage.php gitiwiki~wiki:generateBook manuel-1.1 index
-
-rm -rf books/manuel-1.2
-php doc_fr/scripts/manage.php gitiwiki~wiki:generateBook manuel-1.2 index
-
-rm -rf books/manuel-1.3
-php doc_fr/scripts/manage.php gitiwiki~wiki:generateBook manuel-1.3 index
-
-rm -rf books/manuel-1.4
-php doc_fr/scripts/manage.php gitiwiki~wiki:generateBook manuel-1.4 index
+REPO=repositories/fr/jelix-manuel-fr/
+APP=doc_fr
+update
 
