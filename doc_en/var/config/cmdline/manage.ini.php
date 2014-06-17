1fafce65b069a9ce5186aed244f6e0b904aa08dc
Laurent Jouanneau <laurent@jelix.org>
2014-06-17 13:38:33 +0200

    Fixed configuration

--- doc_en/var/config/cmdline/manage.ini.php
+++ doc_en/var/config/cmdline/manage.ini.php
@@ -10,3 +10,13 @@ gitiwiki.access=2
 gtwdocbook.access=2
 
 [responses]
+
+
+[fileLogger]
+default=messages-manage.log
+error=errors-manage.log
+warning=errors-manage.log
+notice=errors-manage.log
+deprecated=errors-manage.log
+strict=errors-manage.log
+debug=debug-manage.log
